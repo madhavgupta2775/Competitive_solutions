c319ef4e4b3d7df91c4fded0a7561a57abd2b608
nannu <guptamadhav2775@gmail.com>
2022-11-11 21:08:00 +0530

    Add solution for problem `A - Technical Support` Link: https://codeforces.com/contest/1754/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1754/submission/180485973

---
+++ codeforces/1754/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+
+const int MAXN = 200100;
+const int MOD = 1000000007;
+
+void solve() {
+    int n, ans = 0, que = 0; cin >> n;
+    string s; cin >> s;
+    for(int i = n-1; i >= 0; i--){
+        if(s[i] == 'Q'){
+            que++;
+        }
+        else{
+            ans++;
+        }
+        if(ans < que){
+            cout << "no" << endl;
+            return;
+        }
+    }
+    cout << "Yes" << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}
