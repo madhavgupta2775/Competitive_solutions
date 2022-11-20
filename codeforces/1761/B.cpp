4ef63fa4fea42e00278be8f9a697aeed4c74721d
nannu <guptamadhav2775@gmail.com>
2022-11-20 20:45:00 +0530

    Add solution for problem `B - Elimination of a Ring` Link: https://codeforces.com/contest/1761/problem/B Tags: constructive algorithms, greedy, implementation, *1000 Ref: https://codeforces.com/contest/1761/submission/181771569

---
+++ codeforces/1761/B.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+
+const int MAXN = 200100;
+const int MOD = 1000000007;
+
+void solve() {
+    int n, a = 0; cin >> n;
+    vector<int> v(n); bool odd= false;
+    map<int, int> m;
+    forn(i, 0, n){
+        cin >> v[i];
+        a = v[i];
+        m[v[i]]++;
+        if(m.size() > 2){
+            odd = true;
+        }
+    }
+    if(odd){
+        cout << n << endl;
+    }
+    else{
+        cout << n - m[a] + 1 << endl;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}   
