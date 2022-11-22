8fca915fb5b8c3ad3d754040657676b889e7b85e
nannu <guptamadhav2775@gmail.com>
2022-11-22 10:51:00 +0530

    Add solution for problem `A - Medium Number` Link: https://codeforces.com/contest/1760/problem/A Tags: implementation, sortings, *800 Ref: https://codeforces.com/contest/1760/submission/182087371

---
+++ codeforces/1760/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+
+const int MAXN = 200100;
+const int MOD = 1000000007;
+
+void solve() {
+    int a, b, c; cin >> a >> b >> c;
+    vector<int> v(3);
+    v[0] = a; v[1] = b; v[2] = c;
+    sort(v.begin(), v.end());
+    cout << v[1] << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}   
