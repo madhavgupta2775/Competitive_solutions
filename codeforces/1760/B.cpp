dfb163b000b1044c4b8f7fead915520e98a45452
nannu <guptamadhav2775@gmail.com>
2022-11-22 10:56:00 +0530

    Add solution for problem `B - Atilla's Favorite Problem` Link: https://codeforces.com/contest/1760/problem/B Tags: greedy, implementation, strings, *800 Ref: https://codeforces.com/contest/1760/submission/182087707

---
+++ codeforces/1760/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+
+const int MAXN = 200100;
+const int MOD = 1000000007;
+
+void solve() {
+    int n, a = 0, b = 0; cin >> n;
+    string s; cin >> s;
+    forn(i, 0, s.length()){
+        b = s[i] - 'a' + 1;
+        a = max(a, b);
+    }
+    cout << a << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}   
