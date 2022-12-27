bacaa2c5400d0e0b3b9c96a5a12b728483a7ad27
nannu <guptamadhav2775@gmail.com>
2022-12-27 20:09:00 +0530

    Add solution for problem `A - Joey Takes Money` Link: https://codeforces.com/contest/1731/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1731/submission/186887504

---
+++ codeforces/1731/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+ 
+const int MAXN = 200100;
+const int MOD = 998244353;
+ 
+void solve() {
+    int n, a = 0, sum = 1; cin >>n;
+    forn(i, 0, n){
+        cin >> a;
+        sum *= a;
+    }
+    cout << (sum+n-1)*2022 << endl;
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}
