d1e95486d401adaed84c7f096aa3a59963f32424
nannu <guptamadhav2775@gmail.com>
2023-02-18 00:47:00 +0530

    Add solution for problem `A - Yet Another Promotion` Link: https://codeforces.com/contest/1793/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1793/submission/194038457

---
+++ codeforces/1793/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+#define ff first
+#define ss second
+
+const int MOD2 = 1e9+7;
+const int MOD = 998244353;
+
+void solve() {
+    int a, b, n, m; cin >> a >> b >> n >> m;
+    int temp = ((n/(m+1))*m)*a + (n%(m+1))*min(a, b);
+    cout << min(temp, b*n) << endl;
+}   
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    //solve();
+    return 0;
+}
