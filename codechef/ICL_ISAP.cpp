0a8ef06e94944d5567dc8ef0617375f4080d2f59
nannu <guptamadhav2775@gmail.com>
2023-03-13 19:20:28 +0530

    added problem from codechef

---
+++ codechef/ICL_ISAP.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+#define ff first
+#define ss second
+
+const int MOD2 = 1e9+7;
+const int MOD = 998244353;
+
+void solve() {
+    int a, b, n; cin >> a >> b >> n;
+    if((b-a)%(n+1) == 0){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}   
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    //  solve();
+    return 0;
+}
