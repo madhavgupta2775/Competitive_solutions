dfcdb90cca391550f6a3f404f7768b0eca55ec74
nannu <guptamadhav2775@gmail.com>
2023-03-14 20:35:23 +0530

    added problem from codechef

---
+++ codechef/ICL_MKSQR.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+#define ff first
+#define ss second
+
+const int MOD2 = 1e9+7;
+const int MOD = 998244353;
+
+void solve() {
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    int m = min(a, b); m  = min(c, m); m = min(d, m);
+    int sum = a + b + c + d - 4*m;
+    cout << sum << endl;
+}   
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    //  solve();
+    return 0;
+}
