48b152ab09f909e2b53c3fa1d2045aa4feda2182
nannu <guptamadhav2775@gmail.com>
2023-03-22 23:12:27 +0530

    added problem from codechef

---
+++ codechef/OPMIN.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+#define ff first
+#define ss second
+
+const int MOD2 = 1e9+7;
+const int MOD = 998244353;
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n, count = 0; cin >> n;
+    vector<int> v(n); map<int, int> m;
+    forn(i, 0, n){
+        cin >> v[i];
+        m[v[i]]++;
+    }
+    sort(v.begin(), v.end());
+    cout << n - m[v[0]] << endl;
+
+}   
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    // cout << setprecision(30);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    // solve();
+    return 0;
+}
