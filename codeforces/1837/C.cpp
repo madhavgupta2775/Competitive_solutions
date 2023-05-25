497a5b6bf77b65f96d095bf1a93a134a25a46a30
nannu <guptamadhav2775@gmail.com>
2023-05-25 20:25:00 +0530

    Add solution for problem `C - Best Binary String` Link: https://codeforces.com/contest/1837/problem/C Tags: constructive algorithms, greedy Ref: https://codeforces.com/contest/1837/submission/207184600

---
+++ codeforces/1837/C.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+#define ff first
+#define ss second
+
+const int MOD2 = 1e9+7;
+const int MOD = 998244353;
+
+void solve() {
+    string s, ans = ""; cin >> s;
+    forn(i, 0, s.size()){
+        if(i != 0 && s[i] == '?'){
+            ans += ans[i-1];
+        }
+        else if(s[i] == '?'){
+            ans += '0';
+        }
+        else{
+            ans += s[i];
+        }
+    }
+    cout << ans << endl;
+}   
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    // cout << setprecision(30);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}
