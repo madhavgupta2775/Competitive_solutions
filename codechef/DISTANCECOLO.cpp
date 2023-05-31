f0a61d316887ec75a77618c45301a6999e08e530
nannu <guptamadhav2775@gmail.com>
2023-05-31 22:37:31 +0530

    added problem from codechef

---
+++ codechef/DISTANCECOLO.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+#define ff first
+#define ss second
+
+const int MOD2 = 1e9+7;
+// const int MOD = 998244353;
+
+void solve() {
+    int n, k, ans = 1, temp; cin >> n >> k;
+    temp = k;
+    forn(i, 0, n){
+        if(temp > 0){
+            ans *= temp;
+            ans %= MOD2;
+            temp--;
+        }
+    }
+    cout << ans << endl;
+}   
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    // cout << setprecision(30);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}
