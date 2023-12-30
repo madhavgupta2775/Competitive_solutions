de7b2c8b2dd988e7a4089292506be5d2bed100a8
nannu <guptamadhav2775@gmail.com>
2023-12-30 20:39:00 +0530

    Add solution for problem `B - Two Divisors` Link: https://codeforces.com/contest/1916/problem/B Tags: constructive algorithms, math, number theory, *900 Ref: https://codeforces.com/contest/1916/submission/239647160

---
+++ codeforces/1916/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)    
+#define endl "\n" 
+#define int long long
+
+void solve() {
+    int a, b; cin >> a >> b;
+    int ans = lcm(a, b);
+    if(ans == b) {
+        cout << ans * b/a << endl;
+    }
+    else cout << ans << endl;
+}
+
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    // cout << setprecision(30);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve();
+    }
+    return 0;
+}
