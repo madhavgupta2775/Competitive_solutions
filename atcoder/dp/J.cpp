f4fc913a8f3492b4c646ddd1b5f5a583e850d52d
nannu <guptamadhav2775@gmail.com>
2024-01-05 15:13:00 +0530

    Add solution for problem `J - Sushi` Link: https://atcoder.jp/contests/dp/tasks/dp_j Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/49035961

--- atcoder/dp/J.cpp
+++ atcoder/dp/J.cpp
@@ -1,49 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define forn(i,a,n) for(long long i = a; i < n; i++)    
-#define endl "\n" 
-#define int long long
+int n;
+vector<int> v;
+map<int, int> m;
+vector<vector<vector<double> > > t;
 
-double dp[302][302][302]; // i dishes with 1 sushi, j dishes with 2 sushi, k dishes with 3 sushi
-
-double compute(int i, int j, int k, int n) {
-    if(i == 0 && j ==  0 && k == 0) return 0;
-    if(dp[i][j][k] > -0.5) return dp[i][j][k];
-    dp[i][j][k] = 0; double a = 0, b = 0, c = 0;
-    if(i> 0)a = compute(i-1, j, k, n); // expected operations to finish all dishes after choosing a dish with 1 sushi
-    if(j>0)b = compute(i+1, j-1, k, n); // expected operations to finish all dishes after choosing a dish with 2 sushi
-    if(k>0)c = compute(i, j+1, k-1, n); // expected operations to finish all dishes after choosing a dish with 3 sushi
-    double temp = (n/(double)(i + j + k)); 
-    if(i > 0)temp += (i*a)/(double)(i + j + k); 
-    if(j> 0)temp += (j*b)/(double)(i + j + k); 
-    if(k > 0)temp += (k*c)/(double)(i + j + k);
-    dp[i][j][k] = temp;
-    return dp[i][j][k];
-}
-
-void solve() {
-    int n; cin >> n;
-    vector<int> a(n); forn(i,0,n) cin >> a[i];
-    memset(dp, -1, sizeof(dp));
-    int one = 0, two = 0, three = 0;
-    forn(i,0,n) {
-        if(a[i] == 1) one++;
-        else if(a[i] == 2) two++;
-        else three++;
+double Curse(int i, int j, int k, int l)
+{
+    double ans = 0;
+    if (i == n)
+    {
+        return 0;
     }
-    cout << compute(one, two, three, n) << endl;
+    if (t[j][k][l] != -1){
+        return t[j][k][l];
+    }
+    ans += (double)n / (n - i);
+    if (j > 0)
+    {
+        ans += (j*Curse(i + 1, j - 1, k, l)) / (double)(n - i);
+    }
+    if (k > 0)
+    {
+        ans += (k*Curse(i, j + 1, k - 1, l)) / (double)(n - i);
+    }
+    if (l > 0)
+    {
+        ans += (l*Curse(i, j, k + 1, l - 1)) / (double)(n - i);
+    }
+    //cout << ans << " " << i << " " << j << " " << k << " " << l << endl;
+    return t[j][k][l] = ans;
 }
 
- 
-int32_t main() {
+int32_t main()
+{
     ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    cout << setprecision(30);
-    int T = 1;
-    // cin >> T;
-    for(int I = 1; I <= T; I++) {
-        solve();
+    cin.tie(0);
+    cout.precision(15);
+    cin >> n;
+    v.resize(n);
+    t.resize(n+1, vector<vector<double> > (n+1,vector<double> (n+1,-1)));
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        m[v[i]] += 1;
     }
+    cout << Curse(0, m[1], m[2], m[3]) << endl;
     return 0;
 }
