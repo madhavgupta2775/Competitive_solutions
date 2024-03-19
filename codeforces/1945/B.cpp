697d9f2a8a0c0038149bd95ea2625aa3976e7756
nannu <guptamadhav2775@gmail.com>
2024-03-19 13:44:00 +0530

    Add solution for problem `B - Fireworks` Link: https://codeforces.com/contest/1945/problem/B Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1945/submission/252198960

---
+++ codeforces/1945/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define forn(i,a,n) for(long long i = a; i < n; i++)    
+#define endl "\n" 
+#define int long long
+#define ull unsigned long long
+
+const int MOD = 998244353;
+
+void solve() {
+    int a, b, m; cin >> a >> b >> m;
+    int first = m/a + 1, second = m/b + 1;
+    cout << first + second << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // cout << setprecision(30);
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        solve();
+    }
+    return 0;
+}
