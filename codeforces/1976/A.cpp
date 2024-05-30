d849e4ccd88fa5fcfc16616056b85fccb51cb6c2
nannu <guptamadhav2775@gmail.com>
2024-05-30 20:07:00 +0530

    Add solution for problem `A - Verify Password` Link: https://codeforces.com/contest/1976/problem/A Tags: implementation, sortings, strings Ref: https://codeforces.com/contest/1976/submission/263268915

---
+++ codeforces/1976/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,a,n) for(long long i = a; i < n; i++)    
+#define endl "\n" 
+#define int long long
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int n; cin >> n;
+    string s,ans; cin >> s; ans = s;
+    sort(s.begin(), s.end());
+    if(s == ans) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
