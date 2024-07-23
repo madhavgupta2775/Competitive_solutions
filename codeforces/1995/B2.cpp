e08267c7f5c911abcc48091025d11ec62e329c04
nannu <guptamadhav2775@gmail.com>
2024-07-23 20:27:00 +0530

    Add solution for problem `B2 - Bouquet (Hard Version)` Link: https://codeforces.com/contest/1995/problem/B2 Tags: greedy, math, sortings, two pointers Ref: https://codeforces.com/contest/1995/submission/272105866

---
+++ codeforces/1995/B2.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+
+void solve() {
+    int n, m; cin >> n >> m;
+    map<int, int> mp;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].second;
+        mp[a[i].first] = a[i].second;
+    }
+    int ans = 0;
+    for (auto x: mp) {
+        if (x.second == 0) continue;    
+        int a = min(x.second, m / x.first);
+        int b = min((m - a * x.first) / (x.first + 1), mp[x.first + 1]);
+        int c = min(min(m - a * x.first - b * (x.first + 1), mp[x.first + 1] - b), a);
+        ans = max(ans, (a - c) * x.first + (b + c) * (x.first + 1));
+    }
+    cout << ans << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
